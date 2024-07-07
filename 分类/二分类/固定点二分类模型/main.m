76ebd147a337f267d05ba27ee5549662a0d517a3
cloudy2345 <43574872+cloudy2345@users.noreply.github.com>
2024-07-07 14:45:19 +0800

    Update main.m

--- 分类/二分类/固定点二分类模型/main.m
+++ 分类/二分类/固定点二分类模型/main.m
@@ -2,7 +2,7 @@ function [a,b,c,d,cishu]=main(nchangshi)
 a=0;b=0;c=0;d=0;zz=0;
 for k=1:nchangshi
     try
-        [x_train,x_test,y_train,y_test,x_node,y_node]=shuju6();
+        [x_train,x_test,y_train,y_test,x_node,y_node]=dataset6();
         fenlei=model2(x_train,y_train,x_node,y_node)  ; %·µ»Ø·ÖÀàÄ£ÐÍ
         [error_train,error_test,svm_error_train,svm_error_test]=ceshi(fenlei,x_train,x_test,y_train,y_test);
 
