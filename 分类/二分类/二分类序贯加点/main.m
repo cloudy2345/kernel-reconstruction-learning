7a682a25a1e7a096973b3d4dc821cffbe16f3d37
cloudy2345 <43574872+cloudy2345@users.noreply.github.com>
2024-07-07 14:09:16 +0800

    Update main.m

--- 分类/二分类/二分类序贯加点/main.m
+++ 分类/二分类/二分类序贯加点/main.m
@@ -3,15 +3,15 @@ a=0;b=0;c=0;d=0;zz=0;e=0;f=0;
 for k=1:nchangshi
     try
         [x_train,x_test,y_train,y_test,x_node,y_node,node]=shuju6();
-        node2=xunlian8(x_train,y_train,x_node,y_node,node)  ; %d方法加点
-        node3=xunlian7(x_train,y_train,x_node,y_node,node) ;  %p加点法
+        node2=model8(x_train,y_train,x_node,y_node,node)  ; %d方法加点
+        node3=model7(x_train,y_train,x_node,y_node,node) ;  %p加点法
         x_node2=x_train(node2,:);
         y_node2=y_train(node2);
         x_node3=x_train(node3,:);
         y_node3=y_train(node3);
-        fenlei1=xunlian2(x_train,y_train,x_node,y_node)  ; %不加点模型
-        fenlei2=xunlian3(x_train,y_train,x_node2);       %d
-        fenlei3=xunlian2(x_train,y_train,x_node3,y_node3);   %p
+        fenlei1=model2(x_train,y_train,x_node,y_node)  ; %不加点模型
+        fenlei2=model3(x_train,y_train,x_node2);       %d
+        fenlei3=model2(x_train,y_train,x_node3,y_node3);   %p
         [error_train1,error_test1]=ceshi(fenlei1,x_train,x_test,y_train,y_test);
         [error_train2,error_test2]=ceshi(fenlei2,x_train,x_test,y_train,y_test);
         [error_train3,error_test3]=ceshi(fenlei3,x_train,x_test,y_train,y_test);
